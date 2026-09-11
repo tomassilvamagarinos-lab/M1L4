ee8e5f7f1b6e331e44d601656cef626a792d38f9
tomassilvamagarinos-lab <tomas.silvamagarinos@janer.esc.edu.ar>
2026-09-11 20:34:17 -0300

    Refactor bot commands and add meme functionality
    Updated command prefix, added new commands for sending memes and images, and modified password response.

--- bot2.py
+++ bot2.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 from bot_logic import gen_password
-
+import os
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='*', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -21,23 +22,29 @@ async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def no(ctx):
-   await ctx.send("si")
-
-@bot.command()
-async def todo_bien?(ctx)
- await ctx.send("bien, y tu?")
-
-@client.tree.command()
-@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
-async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
-    """Says when a member joined."""
-     member = member or interaction.user
- await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
-
+async def bye(ctx):
+    await ctx.send("\U0001f642")
+    
 @bot.command()
 async def password(ctx):
     password = gen_password()
-    await ctx.send(f'La contraseña generada es: {password}')
+    await ctx.send(f'Tu contraseña es: {password}')
+    
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.png', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    
+@bot.command()
+async def meme(ctx):
+    
+    img_name = random.choice(os.listdir('images'))
+    
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+   
+    await ctx.send(file=picture)
 
 bot.run("key")
